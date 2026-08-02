91905937bf3248206ba7f2b919a04f9d8b2363cb
sevanetrebchenko <seva.netrebchenko@gmail.com>
2026-08-02 14:44:57 -0600

    Added add/remove functions for custom sinks

--- include/utils/logging.hpp
+++ include/utils/logging.hpp
@@ -3,6 +3,8 @@
 #define UTILS_LOGGING_HPP
 
 #include "utils/string.hpp"
+#include <spdlog/fwd.h>
+#include <memory>
 
 namespace utils {
 
@@ -10,6 +12,9 @@ namespace utils {
 
         // Sets the pattern used to format every subsequent log message
         void set_pattern(std::string_view pattern);
+
+        void add_sink(std::shared_ptr<spdlog::sinks::sink> sink);
+        void remove_sink(std::shared_ptr<spdlog::sinks::sink> sink);
 
         template <typename ...Args>
         void info(FormatString fmt, const Args&... args);
--- src/logging.cpp
+++ src/logging.cpp
@@ -1,23 +1,39 @@
 
 #include "utils/logging.hpp"
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/dist_sink.h>
 #include <spdlog/logger.h>
+
+#include <utility>
 
 namespace utils::logging {
 
     namespace detail {
 
-        spdlog::logger create_default_logger() {
+        std::shared_ptr<spdlog::sinks::dist_sink_mt> create_default_sink() {
+            std::shared_ptr<spdlog::sinks::dist_sink_mt> sink = std::make_shared<spdlog::sinks::dist_sink_mt>();
+
             std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-            stdout_sink->set_level(spdlog::level::trace); // Every level is written to stdout
+            stdout_sink->set_level(spdlog::level::trace);  // Everything is written to stdout
+            sink->add_sink(stdout_sink);
 
             std::shared_ptr<spdlog::sinks::stderr_color_sink_mt> stderr_sink = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
-            stderr_sink->set_level(spdlog::level::err); // Only errors are mirrored to stderr
+            stderr_sink->set_level(spdlog::level::err);  // Only errors are written to stderr
+            sink->add_sink(stderr_sink);
 
-            spdlog::logger logger("lightswitch", { stdout_sink, stderr_sink });
+            return sink;
+        }
+
+        std::shared_ptr<spdlog::sinks::dist_sink_mt>& sink() {
+            static std::shared_ptr<spdlog::sinks::dist_sink_mt> instance = create_default_sink();
+            return instance;
+        }
+
+        spdlog::logger create_default_logger() {
+            std::shared_ptr<spdlog::sinks::dist_sink_mt> sink = detail::sink();
+            spdlog::logger logger("lightswitch", { sink });
             logger.set_level(spdlog::level::trace);
             logger.set_pattern("[%l] %v");
-
             return logger;
         }
 
@@ -31,6 +47,16 @@ namespace utils::logging {
     void set_pattern(std::string_view pattern) {
         spdlog::logger& logger = detail::logger();
         logger.set_pattern(std::string(pattern));
+    }
+
+    void add_sink(std::shared_ptr<spdlog::sinks::sink> s) {
+        std::shared_ptr<spdlog::sinks::dist_sink_mt> sink = detail::sink();
+        sink->add_sink(std::move(s));
+    }
+
+    void remove_sink(std::shared_ptr<spdlog::sinks::sink> s) {
+        std::shared_ptr<spdlog::sinks::dist_sink_mt> sink = detail::sink();
+        sink->remove_sink(std::move(s));
     }
 
 }
