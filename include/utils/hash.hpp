58c5f8860c296f3c341c8e00059e6f843d5aec0c
sevanetrebchenko <seva.netrebchenko@gmail.com>
2026-08-02 23:27:24 -0600

    Added FNV-1a string hash implementation

--- include/utils/hash.hpp
+++ include/utils/hash.hpp
@@ -11,6 +11,16 @@ namespace utils {
     
     void hash_combine(std::size_t& seed, const char* value);
     void hash_combine(std::size_t& seed, const char* value, std::size_t length);
+
+    // FNV-1a string hash
+    constexpr std::uint64_t hash_fnv1a(std::string_view name) {
+        std::uint64_t hash = 14695981039346656037ull;
+        for (const char c : name) {
+            hash ^= static_cast<std::uint64_t>(static_cast<unsigned char>(c));
+            hash *= 1099511628211ull;
+        }
+        return hash;
+    }
     
 }
 
